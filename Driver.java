f2ec878fa62782a1090779d7ae7e8816a5958fcf
Alvaro Hernandez <Alvaroh8@nycstudents.net>
2025-02-06 11:25:43 -0500

    set up and finished day 1 _ 1

--- Driver.java
+++ Driver.java
@@ -1,16 +1,33 @@
+import java.util.*;
+import java.io.*;
+
 public class Driver{
   public static void main(String[]args){
-    //Write your tests here!
-    Point r = new Point(100,100);
-    Point p = new Point(3.0,4.0);
-    Point q = new Point(0.0,0.0);
-    
-    System.out.println(p+" should be (3.0,4.0)");
-    
-    System.out.println(r.distance(p,q)+" should be 5.0");
-    
-    p = new Point(3.0,4.0);
-    q = new Point(4.0,5.0);
-    System.out.println(p.distanceTo(q)+" should be 1.4142...");
+    System.out.println(day1("Day1_1.txt"));
+  }
+
+  public static int day1(String filename){
+    try {
+      File file = new File(filename);//1
+      Scanner input = new Scanner(file);
+
+      ArrayList<Integer> fuel = new ArrayList<Integer>();
+      while (input.hasNextInt()){
+        fuel.add(input.nextInt());
+      }
+      int sum = 0;
+      for (int i = 0; i < fuel.size(); i++){
+        int fuelNeeded = ((fuel.get(i) / 3) - 2);
+        sum+= fuelNeeded;
+      }
+
+
+      input.close();//releases the file from your program
+      return sum;
+
+    } catch (FileNotFoundException ex) {
+      //File not found what should you do?
+      System.out.println("File not found");        return 0; //you can return from a void function just don't put a value.
+    }
   }
 }
